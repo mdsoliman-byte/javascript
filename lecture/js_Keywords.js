a31cdc780c955235f67ede61a560b0dd70100e2a
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-03-28 23:44:15 +0600

    javascript keywords ,,

---
+++ lecture/js_Keywords.js
@@ -0,0 +1,3 @@
+// javascript keywords 
+// example : function is a javascript keyword , when i call function keywords than she tells the browser create function 
+// let , function , var , const , etc 
