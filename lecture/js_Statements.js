13cdf00f5f35cfdb41d479438f090caee49f8edc
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-03-28 23:22:28 +0600

    javascript statements

--- lecture/js_Statements.js
+++ lecture/js_Statements.js
@@ -1,2 +1,7 @@
-// javascript Statements ar combenition of values , operators , expression , keywords , comments 
+// javascript Statements ar combenition of values , operators , expression , keywords , comments
 
+// 1) Values
+// 2) operators 
+// 3) Expression
+// 4) Keywords
+// 5) Comments
