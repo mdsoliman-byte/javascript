bd57aaa5e4de68b79fb1134c431247d6be977c50
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-02 21:01:16 +0600

    javascript identifièr , identifier is just a ligle  name

---
+++ lecture/js_Identyfire.js
@@ -0,0 +1,4 @@
+// Javscript identyfire are is a name 
+// example : when we create a function like (function createUser = {}) , after the function kyewoard we using a name like (createUser) its a function identyfire 
+
+
