ba3ce41022fec1a170f554cc45f1625dc26582ed
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-02 20:54:24 +0600

    javscript comments , use dubble quate

---
+++ lecture/js_Comments.js
@@ -0,0 +1,11 @@
+// jsvascript comments are like a  NOTES , create a comments usign (// means dubble slash )
+// js have two type comments 
+        // 1) single line comment 
+        // 2) multi line comments 
+
+// 
+
+// NOTE : THIS SHORT NOTE CREATED MY USIGN SINGLE LINE COMMENTS 
+
+
+
--- lecture/js_Syntax.js
+++ lecture/js_Syntax.js
@@ -2,8 +2,8 @@
 // example : using (var ) create a javascript variable , (var ) is a javascript syntax
 // ***************
 // javascript syntax are two type value ; 1) FIXED VALUE 2) VARIABLE VALUE 
-// 1) FIXED VALUE : var naem = "Shakil"; (note : (Shakil) is a fixed value , fixed value are call LITERALS value )
-// 2) VARIABLE VALUE : var name = {variable name}  (note : variable name is a variable value , variable value are called variables value )
+            // 1) FIXED VALUE : var naem = "Shakil"; (note : (Shakil) is a fixed value , fixed value are call LITERALS value )
+            // 2) VARIABLE VALUE : var name = {variable name}  (note : variable name is a   variable value , variable value are called variables value )
 
 // ****************
 // LITERRALS VALUE : literals value are two type value , NNUMBER VALUE AND STRING VALUE 
