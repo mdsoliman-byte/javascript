e3f67534b28c56075eb3f3bb50809076a1d96fe4
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-05 02:11:29 +0600

    javascript variables , variables have 3typ kyewords , var , let , const

---
+++ lecture/js_variable.js
@@ -0,0 +1,11 @@
+// variable is a container just for value store 
+// example: var num = 10 (var is a kyewords , num is a variable name , = is a operator , 10 is a variable value );
+
+// javascript have 3 type variable kyewords , var, let , const , 
+var num = 10; // var is a variable kyewords , num is a variable name , = is a operator , 10 is variable value 
+let nums = 20; // let is a variable kewords , nums is a variable name , = is a assingment operator , 20 is a variable value 
+const numb = 30; // const is a variable name , numb is a variable name , = is a assingment operator , 30 is variable value 
+
+num = 40;
+
+
