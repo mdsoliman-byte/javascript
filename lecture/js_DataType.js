629ba69a0d2c9dc2e8d592565ea187196367dbdf
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-11 22:07:50 +0600

    javascript object data type

--- lecture/js_DataType.js
+++ lecture/js_DataType.js
@@ -2,20 +2,38 @@
 // string Data type 
 let a = 10 + "Shakil";   //string priority 
 let b = 10 + 4 + "Shakil" //right to left so , first 10+4 = 14 than 
-let c = "Shakil" + 10+ 20; 
+let c = "Shakil" + 10 + 20;
 // console.log(c)  
 
 
 let d;      // undefined value 
-d = 10 ;    // reassigen number value 
-d= "Shakil" // reassign string value , 
+d = 10;    // reassigen number value 
+d = "Shakil" // reassign string value , 
 // javascript can defrent data type data can hold a single variable , dynamic variable 
 // console.log(d)
 
-let e = 10 ;
-let f = 10 ;
-let g = 20 ;
-const restult = (e==f);  // return (true ), so its booleans data type 
-const restult2 = (e==g ); // return (false ), so its booleans data type
-console.log(restult);
-console.log(restult2);
+let e = 10;
+let f = 10;
+let g = 20;
+const restult = (e == f);  // return (true ), so its booleans data type 
+const restult2 = (e == g); // return (false ), so its booleans data type
+// console.log(restult);
+// console.log(restult2);
+
+
+// array data type 
+const arr = [10, 30, 40, 40];
+// console.log(arr[3])
+
+
+
+// object data type 
+const obj = {
+    name: "Shakil",
+    age: 20,
+    address: "Bhola Charfassion ",
+    edu: "CSE "
+}
+
+// console.log(obj)
+console.log(typeof obj );  // check any data type usige typeof 
