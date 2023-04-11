6db17aed1e1d05ca8e024c761d5b8dabe948248c
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-04-11 21:42:13 +0600

    javascript booleans data type , true and false

--- lecture/js_DataType.js
+++ lecture/js_DataType.js
@@ -10,8 +10,12 @@ let d;      // undefined value
 d = 10 ;    // reassigen number value 
 d= "Shakil" // reassign string value , 
 // javascript can defrent data type data can hold a single variable , dynamic variable 
+// console.log(d)
 
-
-
-
-console.log(d)
+let e = 10 ;
+let f = 10 ;
+let g = 20 ;
+const restult = (e==f);  // return (true ), so its booleans data type 
+const restult2 = (e==g ); // return (false ), so its booleans data type
+console.log(restult);
+console.log(restult2);
