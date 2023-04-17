e7c9583ff9d3463eb198fa532d7bf829b58fcbef
Replit user <>
2023-04-17 19:10:19 +0000

    final test commit

--- lecture/js_string.js
+++ lecture/js_string.js
@@ -2,7 +2,7 @@
 console.log("hello");
 console.log("test online editor ")
 const helllo = "hello test";
-const helllo = "hello test";
-const helllo = "hello test";
-const helllo = "hello test";
-const helllo = "hello test";
+const helllo2 = "hello test";
+const helllo22 = "hello test";
+const helllo22222= "hello test";
+const helllo222 = "hello test";
